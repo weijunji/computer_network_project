38571894759274ad34e0bf4e71246f666d0439ed
Haruka <990628wjy@gmail.com>
2018-11-26 09:24:38 +0800

    Finish sk_buff(no test)

---
+++ src/sk_buff.c
@@ -0,0 +1,39 @@
+#include "sk_buff.h"
+
+struct sk_buff* alloc_skb(unsigned int size){
+    struct sk_buff* skb = (struct sk_buff*) malloc(sizeof(struct sk_buff));
+    skb->head = (unsigned char*) malloc(sizeof(unsigned char) * (MAX_TCP_HEADER + size + MAX_TAIL));
+    skb->end = skb->head + (MAX_TCP_HEADER + size + MAX_TAIL);
+    _skb_reserver(skb, MAX_TCP_HEADER);
+    skb->next = NULL;
+    skb->prev = NULL;
+    return skb;
+}
+
+void free_skb(struct sk_buff* skb){
+    free(skb->head);
+    free(skb);
+}
+
+static void _skb_reserver(struct sk_buff *skb, int len){
+    skb->data += len;
+    skb->tail += len;
+}
+
+void skb_put(struct sk_buff *skb, unsigned char* data, unsigned int len){
+    strcpy(skb->tail, data);
+    skb->tail += len;
+    skb->len += len;
+    *(skb->tail) = '\0';
+}
+
+void skb_push(struct sk_buff *skb, unsigned char* data, unsigned int len){
+    skb->data -= len;
+    strcpy(skb->data, data);
+    skb->len += len;
+}
+
+void skb_push(struct sk_buff *skb, unsigned int len){
+    skb->data += len;
+    skb->len -= len;
+}
