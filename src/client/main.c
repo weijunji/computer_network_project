7d64edc1cee93dd196440fea4b45dbabb0dd7913
weijunji <1049918689@qq.com>
2018-12-05 22:54:14 +0800

    finish data link layer

--- src/client/main.c
+++ src/client/main.c
@@ -6,10 +6,22 @@
 
 void physical_layer(char *bit_stream, unsigned int len, char* ip, int port);
 
-// !!! char* 必须以 \0 结尾
+void print_char(const unsigned char* data, int len){
+	for(int i = 0; i < len; i++){
+		if(i != 0 && i % 4 == 0){
+			putchar(' ');
+		}
+		if(data[i] >= 32 && data[i] <= 126){
+			putchar(data[i]);
+		}else{
+			putchar('.');
+		}
+	}
+	putchar('\n');
+}
 
 /* Application layer */
-/*
+
 struct application_layer_header
 {
 	char h;
@@ -17,13 +29,9 @@ struct application_layer_header
 
 void application_layer(char *data, char *dest_ip)
 {
-	struct sk_buff *skb = (struct sk_buff *)malloc(sizeof(struct sk_buff));
-	skb->dest_ip = dest_ip;
-	skb->dest_port = 9628;
-
-	skb_put(skb, (unsigned char*)data, strlen(data));
+	
 }
-*/
+
 /* End of application layer */
 
 /* Transport layer */
@@ -71,19 +79,28 @@ struct ethernet_head{
 
 void data_link_layer(struct sk_buff *skb)
 {
+	printf("\nData link layer:\n");
+	printf("Data: \n");
+	print_char(skb->data, skb->len);
+
 	struct ethernet_head* head = (struct ethernet_head*) malloc(sizeof(struct ethernet_head));
 
-	// unsigned char* dest_mac = (unsigned char*) malloc(sizeof(char) * 6);
 	get_remote_mac(skb->dest_ip, head->dest_mac);  // TODO: PUT TO IP
-	// head->dest_mac = dest_mac;
 
-	// unsigned char* local_mac = (unsigned char*) malloc(sizeof(char) * 6);
 	get_local_mac(head->sour_mac);
-	// head->sour_mac = local_mac;
 
 	head->length = skb->len;
 
-	head->type = 0x0800;
+	head->type = 0x0800; // TODO: GET FROM SKB->PROTOCOL
+
+	printf("Head:");
+	printf("\tdest mac: ");
+	print_mac_address(head->dest_mac);
+	printf("\n\tsour mac: ");
+	print_mac_address(head->sour_mac);
+	printf("\n\tlength: %d\n", head->length);
+	printf("\tprotocol: %04x\n", head->type);
+	printf("\n");
 
 	// 填充
 	if (skb->len < 46){
@@ -93,19 +110,18 @@ void data_link_layer(struct sk_buff *skb)
 	}
 
 	// 前导码 + 定界符
-	unsigned char frame_h[9] = {0};
+	unsigned char frame_h[8] = {0};
 	for (int i = 0; i < 7; i++)
 		frame_h[i] = PREAMBLE;
 	frame_h[7] = SOF;
 
 	unsigned char* headc = (unsigned char*) head;
+
 	skb_push(skb, headc, 16);
-	
-	//skb_push(skb, my_addre, 6);
-	//skb_push(skb, des_addre, 6);
 	skb_push(skb, frame_h, 8);
-	char *bit_stream;
-	bit_stream = (char *)malloc(sizeof(char) * ((skb->len + 4) * 8) + 1);
+
+	unsigned char *bit_stream;
+	bit_stream = (unsigned char *)malloc(sizeof(char) * ((skb->len + 4) * 8));
 
 	for (int i = 0; i < skb->len; i++)
 	{
@@ -121,16 +137,24 @@ void data_link_layer(struct sk_buff *skb)
 		bit_stream[i * 8 + 7] = btb_.bits_.bit1;
 	}
 
+	for(int i = 0; i < skb->len * 8; i++){
+		if(bit_stream[i] != 0){
+			bit_stream[i] = 1;
+		}
+	}
+
 	unsigned long cc;
-	cc = crc32(skb->data, skb->len);
+	cc = crc32(bit_stream, skb->len * 8);
+
+	printf("CRC result: %x\n", cc);
+
 	unsigned char a[4];
 	a[0] = (cc >> 24) & 0xff;
 	a[1] = (cc >> 16) & 0xff;
 	a[2] = (cc >> 8) & 0xff;
 	a[3] = cc & 0xff;
 
-	for (int i = skb->len, j = 0; i < skb->len + 4; i++, j++)
-	{
+	for (int i = skb->len, j = 0; i < skb->len + 4; i++, j++){
 		union btb btb_;
 		btb_.c = a[j];
 		bit_stream[i * 8] = btb_.bits_.bit8;
@@ -143,17 +167,7 @@ void data_link_layer(struct sk_buff *skb)
 		bit_stream[i * 8 + 7] = btb_.bits_.bit1;
 	}
 
-	printf("数据链路层封装好的帧为：\n");
-	/*
-	for (int i = 0; i < (skb->len + 4) * 8; i++){
-		if (bit_stream[i] == 0){
-			bit_stream[i] = -1; // TODO: unsigned -1?
-		}else{
-			bit_stream[i] = 1;
-		}
-	}
-	*/
-	physical_layer(bit_stream, (skb->len + 4) * 8, skb->dest_ip, skb->dest_port);
+	physical_layer((char*)bit_stream, (skb->len + 4) * 8, skb->dest_ip, skb->dest_port);
 	free(bit_stream);
 }
 
@@ -163,48 +177,18 @@ void data_link_layer(struct sk_buff *skb)
 
 void physical_layer(char *bit_stream, unsigned int len, char* ip, int port)
 {
-	/*
-	// convert to bit stream
-	char* bit_stream;
-	bit_stream = (char*) malloc(sizeof(char) * (strlen(data) * 8 + 1));
-	for(int i = 0; i < strlen(data); i++){
-		union btb btb_;
-		btb_.c = data[i];
-		bit_stream[i * 8] = btb_.bits_.bit1;
-		bit_stream[i * 8 + 1] = btb_.bits_.bit2;
-		bit_stream[i * 8 + 2] = btb_.bits_.bit3;
-		bit_stream[i * 8 + 3] = btb_.bits_.bit4;
-		bit_stream[i * 8 + 4] = btb_.bits_.bit5;
-		bit_stream[i * 8 + 5] = btb_.bits_.bit6;
-		bit_stream[i * 8 + 6] = btb_.bits_.bit7;
-		bit_stream[i * 8 + 7] = btb_.bits_.bit8;
-	}
-	printf("Physical layer:\t");
-
-	// char = 0 会导致字符串结尾判断错误，故改为 -1 和 1
-	for(int i = 0; i < strlen(data) * 8; i++){
-		if(bit_stream[i] == 0){
-			printf("0");
-			bit_stream[i] = -1;
-		}else{
-			printf("1");
-			bit_stream[i] = 1;
-		}
-	}
-	printf("\n");
-	bit_stream[strlen(data) * 8] = '\0';
-	*/
-	printf("Physical layer: \n");
+	printf("\nPhysical layer: \n");
+	printf("Send:\n");
 	for(int i = 0; i < len; i++){
-		if (bit_stream[i] == 0){
-			bit_stream[i] = 1;
-			printf("1");
-		}else{
-			bit_stream[i] = 2;
-			printf("0");
-		}
 		if(i != 0 && i % 8 == 0){
 			printf(" ");
+		}
+		if (bit_stream[i] == 0){
+			bit_stream[i] = -1;
+			printf("0");
+		}else{
+			bit_stream[i] = 1;
+			printf("1");
 		}
 	}
 	printf("\n");
@@ -222,11 +206,11 @@ void physical_layer(char *bit_stream, unsigned int len, char* ip, int port)
 /* End of physical layer */
 
 int main(){
-	char *s = "ha";
+	char *s = "skdskdfbksj";
 	// physical_layer(s);
 
 	struct sk_buff *skb = alloc_skb(20);
-	skb_put(skb, (unsigned char*)s, 2);
+	skb_put(skb, (unsigned char*)s, 11);
 	char* ip = "192.168.31.36";
 	skb->dest_ip = ip;
 	skb->dest_port = 9628;
--- src/server/main.c
+++ src/server/main.c
@@ -1,18 +1,35 @@
 ﻿#include "socket_server.h"
 #include <string.h>
+#include "../crc32.h"
+#include "../sk_buff.h"
+#include "../net.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// 打印出可见字符，将不可见字符打印为 .
+void print_char(const unsigned char* data, int len){
+	for(int i = 0; i < len; i++){
+		if(i != 0 && i % 4 == 0){
+			putchar(' ');
+		}
+		if(data[i] >= 32 && data[i] <= 126){
+			putchar(data[i]);
+		}else{
+			putchar('.');
+		}
+	}
+	putchar('\n');
+}
+
+void network_layer(struct sk_buff *skb)
+{
+	printf("\nNetwork layer: \n");
+	printf("Data:\n");
+	print_char(skb->data, skb->len);
+}
 
 /* Data link layer */
 
-void data_link_layer(char* data){
-	printf("Data link layer:\t");
-	printf("%s", data);
-}
-
-/* End of data link layer */
-
-/* Physical layer */
-
-// 通过位域转换二进制
 struct bits{
 	char bit1 : 1;
 	char bit2 : 1;
@@ -24,43 +41,103 @@ struct bits{
 	char bit8 : 1;
 };
 
-union btb{
+union btb {
 	char c;
 	struct bits bits_;
 };
 
+struct ethernet_head{
+	unsigned char dest_mac[6];
+	unsigned char sour_mac[6];
+	unsigned short length;
+	unsigned short type; // 0800 IPv4; 0806 ARP; 86DD IPv6
+};
+
+void data_link_layer(unsigned char *frame, unsigned int len){
+	int i;
+	unsigned long cc;
+
+	printf("\nData link layer:\n");
+	printf("Data:\n");
+	for(int i = 0; i < len; i++){
+		if(i != 0 && i % 8 == 0) printf(" ");
+		printf("%d", frame[i]);
+	}
+	printf("\n");
+
+	cc = crc32(frame, len);
+	printf("CRC result: 0x%x\n", cc);
+	if((int)cc != 0){
+		printf("CRC ERROR: Drop this frame\n");
+		return;
+	}
+	unsigned char* data;
+	data = (unsigned char*) malloc(sizeof(char) * (len - 32) / 8);
+
+	for(i = 0; i < (len-32) / 8; i++){
+		union btb btb_;
+		btb_.bits_.bit8 = frame[i * 8];
+		btb_.bits_.bit7 = frame[i * 8 + 1];
+		btb_.bits_.bit6 = frame[i * 8 + 2];
+		btb_.bits_.bit5 = frame[i * 8 + 3];
+		btb_.bits_.bit4 = frame[i * 8 + 4];
+		btb_.bits_.bit3 = frame[i * 8 + 5];
+		btb_.bits_.bit2 = frame[i * 8 + 6];
+		btb_.bits_.bit1 = frame[i * 8 + 7];
+		data[i] = btb_.c;
+	}
+
+	struct ethernet_head* head = (struct ethernet_head*) malloc(sizeof(struct ethernet_head));
+	memcpy((char*)head, data + 8, 16);
+	printf("Head:");
+	printf("\tdest mac: ");
+	print_mac_address(head->dest_mac);
+	printf("\n\tsour mac: ");
+	print_mac_address(head->sour_mac);
+	printf("\n\tlength: %d\n", head->length);
+	printf("\tprotocol: %04x\n", head->type);
+
+	unsigned char my_addr[6];
+	get_local_mac(my_addr);
+	unsigned char broadcast[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+	if(memcmp((char*)head->dest_mac, (char*)my_addr, 6) != 0 && memcmp((char*)head->dest_mac, (char*)broadcast, 6) != 0){
+		printf("此帧目的地址非本机，舍去\n");
+		return;
+	}
+
+	struct sk_buff *skb = alloc_skb(head->length);
+	skb_put(skb, data + 24, head->length);
+
+	network_layer(skb);
+	free(data);
+}
+
+/* End of data link layer */
+
+/* Physical layer */
+
 void physical_layer(char* data){
-	int size = strlen(data);
+	int size = strlen(data) - 1;
 	char* updata;
 	updata = (char*) malloc(sizeof(char) * (size / 8 + 1));
 
-	printf("Physical layer:\t");
+	printf("Physical layer:\n");
+	printf("Length: %d\n", size);
+	printf("Data:\n");
 	// 将-1转为0
 	for(int i = 0; i < size; i++){
+		if(i != 0 && i % 8 == 0){
+			printf(" ");
+		}
 		if(data[i] == -1){
 			printf("0");
 			data[i] = 0;
+		}else{
+			printf("1");
 		}
-		else printf("1");
 	}
 	printf("\n");
-	for(int i = 0; i < size / 8; i++){
-		union btb btb_;
-		btb_.bits_.bit1 = data[i * 8];
-		btb_.bits_.bit2 = data[i * 8 + 1];
-		btb_.bits_.bit3 = data[i * 8 + 2];
-		btb_.bits_.bit4 = data[i * 8 + 3];
-		btb_.bits_.bit5 = data[i * 8 + 4];
-		btb_.bits_.bit6 = data[i * 8 + 5];
-		btb_.bits_.bit7 = data[i * 8 + 6];
-		btb_.bits_.bit8 = data[i * 8 + 7];
-		updata[i] = btb_.c;
-	}
-	updata[size / 8] = '\0';
-
-	printf("up");
-	data_link_layer(updata);
-	free(updata);
+	data_link_layer(data, size);
 }
 
 /* End of physical layer */
